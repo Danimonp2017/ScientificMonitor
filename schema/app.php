87175a0c70177b412c4c9ad5b25275b8e1b4098e
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-06-01 11:18:43 -0500

    Schemas and sample documents created for all potential categories

--- schema/app.php
+++ schema/app.php
@@ -1,12 +1,34 @@
 use Eloquent\Model as Eloquent;
 
 class País extends Eloquent {}
-
-Schema::create('países', function($collection))
-{
-  $collection -> string('nombre');
-  $collection -> obj('población');
-  $collection -> obj('PIB');
-  $collection -> obj('Gasto_C_T');
-  $collection -> obj('Gasto_I_D');
+class IE extends Eloquent {
+  protected $collection = 'IIEE';
 }
+
+Schema::create('países', function($collection) {
+  $collection -> string('nombre');
+  $collection -> string('moneda_local');
+  $collection -> double('nombre');
+  $collection -> json('población');
+  $collection -> json('PIB');
+  $collection -> json('Gasto_C_T');
+  $collection -> json('Gasto_I_D');
+});
+
+Schema::create('IIEE', function($collection) {
+  $collection -> string('nombre');
+  $collection -> integer('estudiantes');
+  $collection -> boolean('privada');
+  $collection -> double('tasa_empleabilidad');
+  $collection -> boolean('ISO_9001');
+});
+
+Schema::create('facultades', function($collection){
+  $collection -> string('nombre');
+  $collection -> integer('estudiantes');
+  $collection -> json('docentes');
+  $collection -> double('docentes_planta');
+  $collection -> json('programas');
+  $collection -> json('programas_acreditados');
+  $collection -> json('grupos_investigación');
+});
